0435c6ea9dbc1d418b5034c2bdf430febd8eec57
Anbuselvan04 <adsanbuselvan@gmail.com>
2024-07-02 22:18:29 +0530

    Success

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://Anbuselvan04.github.io/simple-stop-watch'
 })
+
